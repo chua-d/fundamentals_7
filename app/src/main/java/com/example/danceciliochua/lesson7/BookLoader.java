5cc43596e189deefc4f65244c9abf9e2ebeff3e7
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-17 18:34:57 +0800

    Finish Lesson 7.2

---
+++ app/src/main/java/com/example/danceciliochua/lesson7/BookLoader.java
@@ -0,0 +1,26 @@
+package com.example.danceciliochua.lesson7;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    public BookLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+}
--- app/src/main/java/com/example/danceciliochua/lesson7/MainActivity.java
+++ app/src/main/java/com/example/danceciliochua/lesson7/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.danceciliochua.lesson7;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.LoaderManager;
 import android.content.Context;
+import android.support.v4.content.Loader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     private EditText mBookInput;
     private TextView mTitle;
@@ -25,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         mBookInput = (EditText) findViewById(R.id.bookInput);
         mTitle = (TextView) findViewById(R.id.titleText);
         mAuthor = (TextView) findViewById(R.id.authorText);
+
+        if(getSupportLoaderManager().getLoader(0) != null) {
+            getSupportLoaderManager().initLoader(0,null,this);
+        }
 
     }
 
@@ -40,9 +50,11 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
         if(networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
-            new FetchBook(mTitle, mAuthor).execute(queryString);
             mAuthor.setText("");
             mTitle.setText(R.string.loading);
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
 
         } else {
             if(queryString.length() == 0) {
@@ -57,6 +69,62 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        Log.d("A", "Start");
+        return new BookLoader(this, args.getString("queryString"));
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            for(int i = 0; i<itemsArray.length();i++) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                String title=null;
+                String authors=null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+                JSONArray authorList = volumeInfo.getJSONArray("authors");
+
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = "";
+                    for(int j=0; j<authorList.length(); j++) {
+                        authors = authors + "\n" + authorList.getString(j);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+                if(title != null && authors != null) {
+                    mTitle.setText(title);
+                    mAuthor.setText(authors);
+                    return;
+                }
+            }
+
+            mTitle.setText("No Results Found");
+            mAuthor.setText("");
+
+        } catch (Exception e) {
+            mTitle.setText("No Results Found");
+            mAuthor.setText("");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
 
     }
 }

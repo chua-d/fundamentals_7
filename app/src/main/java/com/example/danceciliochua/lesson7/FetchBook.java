f155f13f88292833843cd8a54511cf6b5a889763
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-17 17:23:20 +0800

    Finish Lesson 7.2A

--- app/src/main/java/com/example/danceciliochua/lesson7/FetchBook.java
+++ app/src/main/java/com/example/danceciliochua/lesson7/FetchBook.java
@@ -3,6 +3,9 @@ package com.example.danceciliochua.lesson7;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class FetchBook extends AsyncTask<String, Void, String> {
 
     private TextView mTitleText;
@@ -16,12 +19,54 @@ public class FetchBook extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... strings) {
-        return null;
+        return NetworkUtils.getBookInfo(strings[0]);
     }
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            for(int i = 0; i<itemsArray.length();i++) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                String title=null;
+                String authors=null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+                JSONArray authorList = volumeInfo.getJSONArray("authors");
+
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = "";
+                    for(int j=0; j<authorList.length(); j++) {
+                        authors = authors + "\n" + authorList.getString(j);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+                if(title != null && authors != null) {
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                    return;
+                }
+            }
+
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+
+        } catch (Exception e) {
+            mTitleText.setText("No Results Found");
+            mAuthorText.setText("");
+            e.printStackTrace();
+
+        }
+
     }
 
 }
--- app/src/main/java/com/example/danceciliochua/lesson7/MainActivity.java
+++ app/src/main/java/com/example/danceciliochua/lesson7/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.danceciliochua.lesson7;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -26,6 +30,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void searchBooks(View view) {
         String queryString = mBookInput.getText().toString();
+
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        mTitle.setText(R.string.loading);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
+            new FetchBook(mTitle, mAuthor).execute(queryString);
+            mAuthor.setText("");
+            mTitle.setText(R.string.loading);
+
+        } else {
+            if(queryString.length() == 0) {
+                mAuthor.setText("");
+                mTitle.setText("Please enter a search term");
+
+            } else {
+                mAuthor.setText("");
+                mTitle.setText("Please check your network connection and try again.");
+
+            }
+
+        }
+
 
     }
 }

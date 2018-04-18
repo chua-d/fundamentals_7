acc6c836717d767eadac44b0eefc1aa3f918396c
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-18 08:44:54 +0800

    Reset Project

--- app/src/main/java/com/example/danceciliochua/lesson7/MainActivity.java
+++ app/src/main/java/com/example/danceciliochua/lesson7/MainActivity.java
@@ -9,23 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mBookInput;
-    private TextView mTitle;
-    private TextView mAuthor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mBookInput = (EditText) findViewById(R.id.bookInput);
-        mTitle = (TextView) findViewById(R.id.titleText);
-        mAuthor = (TextView) findViewById(R.id.authorText);
-
-    }
-
-    public void searchBooks(View view) {
-        String queryString = mBookInput.getText().toString();
 
     }
 }
